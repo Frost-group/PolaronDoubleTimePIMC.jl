598aaf95a5d609f54b853d1be938bce5d3924142
Documenter.jl <documenter@juliadocs.github.io>
2023-07-12 11:59:38 +0000

    build based on 4900c62

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PolaronDoubleTimePIMC","category":"page"},{"location":"#PolaronDoubleTimePIMC","page":"Home","title":"PolaronDoubleTimePIMC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PolaronDoubleTimePIMC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PolaronDoubleTimePIMC]","category":"page"}]
+}
